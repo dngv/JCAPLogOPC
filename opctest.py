659b3b31b4b393bd02fa42927749d42b62f1d78a
Dan Guevarra <beatspace9@gmail.com>
2014-07-10 10:13:06 -0700

    This works, but smtpvars and opcvars contain sensitive info--so don't track.

--- opctest.py
+++ opctest.py
@@ -1,19 +1,43 @@
-## Simple test script for Matrikon OPC simulation server
-## https://www.matrikonopc.com/downloads/178/index.aspx
-
-import OpenOPC
 from time import sleep
+from smtpvars import * # gitignore email credentials
+from opcvars import * # gitignore opc server stuff
+import OpenOPC
 
-opc = OpenOPC.client()
-opc.connect('Matrikon.OPC.Simulation.1')
+opc = OpenOPC.client() # DCOM is faster and .close() doesn't reset this binding
+# opc = OpenOPC.open_client(gateway_host)
 
-def readloop(tag, count=1, delay=1):
-	i=0
-	while i < count:
-		print opc.read(tag)
+# check fault loop, short delay but only read 1 tag
+def faultloop(delay=1): # delay in seconds
+	v=False
+	while not v:
+		opc.connect(opc_server)
+		v, s, t=opc.read(faulttag)
+		opc.close()
+		print v
 		sleep(delay)
-		i+=1
+	mailalert()
+	
+def mailalert(to=to, subject=subject):
+	smtpserv.ehlo()
+	smtpserv.starttls()
+	smtpserv.ehlo
+	smtpserv.login(mailuser, mailpass)
+	header = 'To:' + to + '\n' + 'From: ' + mailuser + '\n' + subject + '\n'
+	msg = header + '\n this is test msg from furnace \n\n'
+	smtpserv.sendmail(mailuser, to, msg)
+	print 'Alert message sent to: ' + mailuser
+	smtpserv.close()
+	
 
-readloop('Random.ArrayOfReal8', 1000)
+#def readalarms(delay=15): # delay in seconds
+#	while i < count:
+#		opc.connect(opc_server)
+#		stuff=opc.read(taglist)
+#		opc.close()
+#		d={}
+#		for tag in stuff:
+#			d[tag[0]]=tag[1]
+#		print d
+#		sleep(delay)
+#		i+=1
 
-opc.close()
