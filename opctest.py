0c9f94c2e4ec4b61de1a3e1175b53a49631478bb
Dan Guevarra <beatspace9@gmail.com>
2014-07-10 17:51:31 -0700

    Implented main loop for continuous monitoring. Stat loop is a placeholder for time between interlock and clear signal. Next update will check actual run in progress before sending the alert message. May need to accomodate asynchronous timers for fault loop and real stat loop.

--- opctest.py
+++ opctest.py
@@ -2,42 +2,84 @@ from time import sleep
 from smtpvars import * # gitignore email credentials
 from opcvars import * # gitignore opc server stuff
 import OpenOPC
+import smtplib
+
+def readtagd(csv): # csv format: tag, type, description, normal value (optional)
+	f=open(csv, mode='r')
+	rawlist=f.readlines()
+	f.close
+	d={}
+	tags=[]
+	for line in rawlist: # csv has no headers
+		v=line.split(',')
+		tag='PVI.PLC.'+v[0]
+		dtype=v[1]
+		desc=v[2].strip('\n')
+		vlist=[dtype, desc]
+		if len(v)>3: # for alarm booleans
+			vlist.append(bool(int(v[3].strip('\n'))))
+		d[tag]=vlist
+	return(d)
+
+alarmd=readtagd('alarmtags.csv')
+statsd=readtagd('statustags.csv')
+faulttag='PVI.PLC.Fault'
 
 opc = OpenOPC.client() # DCOM is faster and .close() doesn't reset this binding
-# opc = OpenOPC.open_client(gateway_host)
 
+def mainloop(dontstop=True):
+	while dontstop:
+		faultloop()
+		statloop()
+		sleep(5)
+	
 # check fault loop, short delay but only read 1 tag
-def faultloop(delay=1): # delay in seconds
+def faultloop(delay=5): # delay in seconds
 	v=False
 	while not v:
 		opc.connect(opc_server)
 		v, s, t=opc.read(faulttag)
 		opc.close()
-		print v
+		if v:
+			mailalert()
+			print 'Alert message sent to: ' + to
+			break
 		sleep(delay)
-	mailalert()
+
+def statloop(delay=10):
+	v=True
+	while v:
+		opc.connect(opc_server)
+		v, s, t=opc.read(faulttag)
+		opc.close()
+		if not v:
+			mailalert(body='Last interlock cleared. \n')
+			break
+		sleep(delay)
+
+def readalarms():
+	opc.connect(opc_server)
+	alarms=opc.read(alarmd.keys())
+	opc.close()
+	alarmlist=[]
+	for i in alarms:
+		key, val, stat, t = i
+		if not alarmd[key][2]==val:
+			alarmlist.append(alarmd[key][1])
+	alarmtext='\nThe following alarms are active: \n' + '\n'.join(alarmlist)
+	return(alarmtext)
 	
-def mailalert(to=to, subject=subject):
+def mailalert(to=to, subject=subject, body=readalarms()):
+	smtpserv = smtplib.SMTP(smtphost, 587)
 	smtpserv.ehlo()
 	smtpserv.starttls()
 	smtpserv.ehlo
 	smtpserv.login(mailuser, mailpass)
 	header = 'To:' + to + '\n' + 'From: ' + mailuser + '\n' + subject + '\n'
-	msg = header + '\n this is test msg from furnace \n\n'
+	msg = header + body
 	smtpserv.sendmail(mailuser, to, msg)
-	print 'Alert message sent to: ' + mailuser
 	smtpserv.close()
-	
 
-#def readalarms(delay=15): # delay in seconds
-#	while i < count:
-#		opc.connect(opc_server)
-#		stuff=opc.read(taglist)
-#		opc.close()
-#		d={}
-#		for tag in stuff:
-#			d[tag[0]]=tag[1]
-#		print d
-#		sleep(delay)
-#		i+=1
+
+	
 
