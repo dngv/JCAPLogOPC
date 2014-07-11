58d1cb164ace0d49b103288ecc83220d08019e6f
dan <guevarra@caltech.edu>
2014-07-11 10:51:48 -0700

    Use single loop with fault, idle, and stat checking based on cycles

--- opctest.py
+++ opctest.py
@@ -4,72 +4,96 @@ from opcvars import * # gitignore opc server stuff
 import OpenOPC
 import smtplib
 
+
 def readtagd(csv): # csv format: tag, type, description, normal value (optional)
-	f=open(csv, mode='r')
-	rawlist=f.readlines()
+	f = open(csv, mode = 'r')
+	rawlist = f.readlines()
 	f.close
-	d={}
-	tags=[]
+	d = {}
+	tags = []
 	for line in rawlist: # csv has no headers
-		v=line.split(',')
-		tag='PVI.PLC.'+v[0]
-		dtype=v[1]
-		desc=v[2].strip('\n')
-		vlist=[dtype, desc]
-		if len(v)>3: # for alarm booleans
+		v = line.split(',')
+		tag = 'PVI.PLC.' + v[0]
+		dtype = v[1]
+		desc = v[2].strip('\n')
+		vlist = [dtype, desc]
+		if len(v) > 3: # for alarm booleans
 			vlist.append(bool(int(v[3].strip('\n'))))
-		d[tag]=vlist
+		d[tag] = vlist
 	return(d)
 
-alarmd=readtagd('alarmtags.csv')
-statsd=readtagd('statustags.csv')
-faulttag='PVI.PLC.Fault'
+alarmd = readtagd('alarmtags.csv')
+statsd = readtagd('statustags.csv')
+faulttag = 'PVI.PLC.Fault'
+runtag = 'PVI.PLC.Run'
 
 opc = OpenOPC.client() # DCOM is faster and .close() doesn't reset this binding
 
-def mainloop(dontstop=True):
-	while dontstop:
-		faultloop()
-		statloop()
-		sleep(5)
+def mainloop(cyctime = 1, faultcy = 5, statcy = 10, nostop = True):
+    c = 0 # init cycle counter
+    run = False # init run in progress
+    fault = False # init global fault
+    mesg = False # init alert sent
+    clist = [faultcy, statcy]
+    flist = ['fault, run, mesg = faultchk(fault, run, mesg)', 'statchk()']
+    while nostop:
+        if run:
+            blist = map(lambda(x): c % x == 0, clist)
+            for i in range(len(blist)):
+                if blist[i]:
+                    eval(flist[i])
+            if c == lcmm(*clist):
+                c = 1
+            else:
+                c += 1
+        else:
+            run = runchk(run)
+        sleep(cyctime)
 	
-# check fault loop, short delay but only read 1 tag
-def faultloop(delay=5): # delay in seconds
-	v=False
-	while not v:
-		opc.connect(opc_server)
-		v, s, t=opc.read(faulttag)
-		opc.close()
-		if v:
-			mailalert()
-			print 'Alert message sent to: ' + to
-			break
-		sleep(delay)
+def runchk(run): # loop when system is idle
+    opc.connect(opc_server)
+    run, state, time = opc.read(runtag)
+    opc.close()
+    if not run:
+        mailalert(body = 'Run has finished. \n')
+    return(run)
 
-def statloop(delay=10):
-	v=True
-	while v:
-		opc.connect(opc_server)
-		v, s, t=opc.read(faulttag)
-		opc.close()
-		if not v:
-			mailalert(body='Last interlock cleared. \n')
-			break
-		sleep(delay)
+def faultchk(fault, run, mesg): # loop when system is running
+    opc.connect(opc_server)
+    fault, state, time = opc.read(faulttag)
+    run, state, time = opc.read(runtag)
+    opc.close()
+    if fault & (not mesg):
+        mailalert()
+        mesg = true
+        print 'Alert message sent to: ' + to
+    if mesg & (not fault):
+        mailalert(body = 'Last interlock cleared. \n')
+        mesg = false
+        print 'Interlock clear message sent to: ' + to
+    return(fault, run, mesg)
+    
+def statchk(): # print furnace stats
+    opc.connect(opc_server)
+    stats = opc.read(statsd.keys())
+    opc.close()
+    for i in stats:
+        key, val, stat, t = i
+        print statsd[key][1] + ': ' + val
 
 def readalarms():
 	opc.connect(opc_server)
-	alarms=opc.read(alarmd.keys())
+	alarms = opc.read(alarmd.keys())
 	opc.close()
-	alarmlist=[]
+	alarmlist = []
 	for i in alarms:
 		key, val, stat, t = i
-		if not alarmd[key][2]==val:
+		if not alarmd[key][2] == val:
 			alarmlist.append(alarmd[key][1])
-	alarmtext='\nThe following alarms are active: \n' + '\n'.join(alarmlist)
+	alarmtext = '\nThe following alarms are active: \n' + '\n'.join(alarmlist)
 	return(alarmtext)
 	
-def mailalert(to=to, subject=subject, body=readalarms()):
+def mailalert(to = to, subject = subject, body = readalarms()):
 	smtpserv = smtplib.SMTP(smtphost, 587)
 	smtpserv.ehlo()
 	smtpserv.starttls()
@@ -80,6 +104,40 @@ def mailalert(to=to, subject=subject, body=readalarms()):
 	smtpserv.sendmail(mailuser, to, msg)
 	smtpserv.close()
 
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:      
+        a, b = b, a % b
+    return a
 
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+def lcmm(*args):
+    """Return lcm of args."""   
+    return reduce(lcm, args)
 	
+# check fault loop, short delay but only read 1 tag -- DEPRECATED
+#def faultloop(delay = 5): # delay in seconds
+#	v = False
+#	while not v:
+#		opc.connect(opc_server)
+#		v, s, t = opc.read(faulttag)
+#		opc.close()
+#		if v:
+#			mailalert()
+#			print 'Alert message sent to: ' + to
+#			break
+#		sleep(delay)
 
+#def statloop(delay = 10): # -- DEPRECATED
+#	v = True
+#	while v:
+#		opc.connect(opc_server)
+#		v, s, t = opc.read(faulttag)
+#		opc.close()
+#		if not v:
+#			mailalert(body = 'Last interlock cleared. \n')
+#			break
+#		sleep(delay)
